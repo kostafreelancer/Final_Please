84ed757b926bca52bf05874ee6b6b5bf9db88eb7
sungjun87 <jakdoo87@naver.com>
2017-06-13 00:31:34 +0900

    된다~

--- src/main/webapp/WEB-INF/views/c_common/server.js
+++ src/main/webapp/WEB-INF/views/c_common/server.js
@@ -5,21 +5,22 @@ var express = require('express')
   , io = require('socket.io').listen(server)
  /* ,  url = require('url')*/
 server.listen(8000);
-
+var username;
 app.get('/', function (req, res) {
 	  
 	 
 	  res.sendfile(__dirname + '/chatting.html');
 });
 app.get('/client',function(req, res){
-	console.log("여기 클라이언트 오는거지?");
-	console.log("쓰바");
-	console.log(req.param("username")+"모라도 찍혀라");
-	  res.sendfile(__dirname + '/client.html');
+	/*console.log("여기 클라이언트 오는거지?");
+	console.log(req.param("username")+"모라도 찍혀라");*/
+	username = req.param("username");
+	res.sendfile(__dirname + '/client.html');
 });
 io.sockets.on('connection', function (socket) {
+	console.log("emit되는거지?"+username);
+	socket.emit('sendusername',username);
 	
-	console.log("접속은 되는거지.");
 	socket.on('sendchat',function(data){
 		io.sockets.emit('updatechat',socket.username,data);
 		console.log(data+': '+socket.username);
